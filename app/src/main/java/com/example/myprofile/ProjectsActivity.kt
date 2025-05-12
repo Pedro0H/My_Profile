796c1417cb9d3de1321ed70edce4cdd7ab7ca90d
Pedro Lima Pereira <91159497+Pedro0H@users.noreply.github.com>
2025-05-12 07:28:41 -0300

    Update ProjectsActivity.kt

--- app/src/main/java/com/example/myprofile/ProjectsActivity.kt
+++ app/src/main/java/com/example/myprofile/ProjectsActivity.kt
@@ -30,22 +30,22 @@ class ProjectsActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(intent)
         }
-/*
+
         val gitText = findViewById<TextView>(R.id.agenda_text)
         gitText.setOnClickListener {
-            val url = ""
+            val url = "https://github.com/Pedro0H/Agenda_App"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(intent)
         }
 
         val gitText = findViewById<TextView>(R.id.imc_text)
         gitText.setOnClickListener {
-            val url = ""
+            val url = "https://github.com/Pedro0H/IMC_Calculator"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(intent)
         }
 
-        */
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
